46d247b4f3e1811e12aac4335630e88612047032
toufiquehasan07 <toufiquehasan07@gmail.com>
2023-05-12 21:34:02 +0600

    React project environment setup with webpack

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, "src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    allowedHosts: "all",
+    compress: true,
+    port: 3000, // default 8000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svg$/i,
+        use: "svg-inline-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$|jsx/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+    }),
+  ],
+  mode: process.env.NODE_ENV == "production" ? "production" : "development",
+};
